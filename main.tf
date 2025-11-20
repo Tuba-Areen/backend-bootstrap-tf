b871b7e5ba583216d1feceea559f7c3a315113e1
Tuba Areen <tubaareen00@gmail.com>
2025-11-20 12:00:00 +0530

    dynamo-db

--- main.tf
+++ main.tf
@@ -31,6 +31,17 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+
 resource "aws_dynamodb_table" "tf_locks" {
   name         = "terraform-lock-${var.project}-${var.environment}"
   billing_mode = "PAY_PER_REQUEST"
--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,3 @@ output "lock_table_name" {
   value = aws_dynamodb_table.tf_locks.name
 }
 
-output "state_bucket_name" {
-  value = aws_s3_bucket.tf_state.bucket
-}
